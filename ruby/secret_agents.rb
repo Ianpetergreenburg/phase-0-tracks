a2925da4ebb7347e5cd6461efa0ed3009346b30c
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-10-28 12:59:42 -0700

    shortening code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,6 +1,5 @@
 def encrypt(secret)
-password_length = secret.length - 1
-	for i in 0..password_length
+	for i in 0...secret.length
 		temp = secret[i]
 		if temp == "z"
 			temp = "a"
@@ -13,9 +12,8 @@ return secret
 end
 
 def decrypt(secret)
-password_length = secret.length - 1
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-	for i in 0..password_length
+	for i in 0...secret.length
 		num = alphabet.index(secret[i])
 		num -= 1
 		secret[i] = alphabet[num]

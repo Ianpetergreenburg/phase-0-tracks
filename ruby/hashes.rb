d8d8c749526fbffef8506a224a622144c92773e0
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-10-31 15:25:02 -0700

    initalizing hashes file, beginning pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,23 @@
+#In phase-0-tracks/ruby/hashes.rb, pseudocode and
+# write a program that will allow an interior designer
+# to enter the details of a given client: the
+# client's name, age, number of children,
+# decor theme, and so on (you can choose your own
+# as long as it's a good mix of string, integer,
+# and boolean data
+
+#create a blank hash
+#have client answer questions with correct data type
+
+
+client_info {
+	name: "",
+	age: nil,
+	has_pets: false,
+	number_children: nil,
+	decor_theme: ""
+	dislikes: ""
+	fav_color: ""
+
+
+}

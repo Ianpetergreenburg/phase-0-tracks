d9af818693591ecc5514ee0b75018c65ebdabb41
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-01 11:20:56 -0700

    adding user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,23 +2,20 @@ def codename(real_name)
 vowels = "aeiou"
 consonants = "bcdfghjklmnpqrstvxz"
 arr = real_name.split(' ')
-temp = arr[1]
-arr[1] = arr[0]
-arr[0] = temp
+arr = arr.reverse()
 switch_real = arr.join(' ').downcase
-puts switch_real
 for i in 0...switch_real.length
-	temp2 = switch_real[i]
-	if vowels.include? temp2
-		num = vowels.index(temp2)
+	temp = switch_real[i]
+	if vowels.include? temp
+		num = vowels.index(temp)
 		if num == vowels.length() - 1 
 			num = 0
 		else
 			num += 1 
 		end
 		switch_real[i] = vowels[num]
-	elsif consonants.include? temp2
-		num = consonants.index(temp2)
+	elsif consonants.include? temp
+		num = consonants.index(temp)
 		if num == consonants.length() - 1 
 			num = 0
 		else
@@ -28,7 +25,20 @@ for i in 0...switch_real.length
 	end
 end
 switch_real = switch_real.split(' ').map! {|x| x.capitalize}.join(' ')
-puts switch_real
+return switch_real
 end
 
-codename("Ian Peter")
+alias_database = {}
+answer = "space"
+until answer.empty? || answer == "quit"
+	puts "Please input first and last name to aquire alias. If you don't need another alias type quit or just leave your answer blank."
+	answer = gets.chomp
+	if (not answer.empty?) && answer != "quit"
+		code = codename(answer)
+		alias_database.store(answer,code)
+	end
+end
+
+if not alias_database.empty?
+	alias_database.each{|key, value| puts key + " will now be referred to as " + value}
+end

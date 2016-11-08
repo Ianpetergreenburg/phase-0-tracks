1e9b5e61b7a0b88a4715458c745496a4c2a1ee63
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-07 23:13:26 -0800

    adding stuff to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,6 @@
 class Santa
-
+attr_reader  :ethnicity
+attr_accessor :gender, :age
   def speak
      puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -9,12 +10,22 @@ class Santa
   end
 
   def initialize(gender, ethnicity)
-     puts "Initializing Santa instance..."
+     #puts "Initializing Santa instance..."
      @gender = gender
      @ethnicity = ethnicity
      @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
      @age = 0
   end
+
+  def celebrate_birthday
+    @age += 1
+end
+
+def get_mad_at (reindeer)
+  @reindeer_ranking.delete(reindeer)
+  @reindeer_ranking << reindeer
+end
+
 end
 
 # chris = Santa.new
@@ -23,7 +34,18 @@ end
 # puts Santa.coat_trim + " was there"
 
 santas = []
+santa_amount = 200
+
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_ethnicities.each_index {|x| santas << Santa.new(example_genders[x], example_ethnicities[x])}
-p santas
+santas[0].gender = "white"
+santas[0].celebrate_birthday
+p santas[0].age
+p santas[0]
+
+santa_amount.times do |x|
+  new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  new_santa.age = rand(140)
+  puts "This Santa is " + new_santa.ethnicity + " and " + new_santa.gender+ " and " + new_santa.age.to_s + " year(s) old."
+end

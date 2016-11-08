17bc47bd8bd0444d86aad5d59d582eb9a7726706
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-07 21:31:29 -0800

    describinf Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#puppy class
+#-----------------------
+#Name
+#-Puppy
+#-----------------------
+# Physical Characteristics
+# -fur color: varies
+# -fur length: varies
+# -floppy ears?: yes/no
+# -paw size: varies
+# -potty trained: yes/no
+#----------------------
+# Behavior
+# -fetch
+# -nap
+# -play

47157ae3d9aa83599b6bdac07d5f29ea13212a99
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-10 15:56:08 -0800

    testing

--- ruby/faker-test.rb
+++ ruby/faker-test.rb
@@ -1,7 +1,8 @@
 require 'faker'
-#require 'libhunspell'
-require 'ffi'
-require 'hunspell-ffi'
+# require 'libhunspell'
+# require 'ffi'
+# require 'hunspell-ffi'
+require 'ffi/hunspell'
 
 # dict = Hunspell.new("C:/Rails Installer/Hunspell/", "en_US")
 # p dict.spell("walked")

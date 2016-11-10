072566e8043842d6a0c2b9bc73bada73d9b6a6a9
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-10 10:59:09 -0800

    small changes

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,23 +1,23 @@
 class TodoList
 
-def initialize(itemarr)
-  @TodoArr = itemarr
-end
+  def initialize(itemarr)
+    @TodoArr = itemarr
+  end
 
-def add_item(item)
-  @TodoArr << item
-end
+  def add_item(item)
+    @TodoArr << item
+  end
 
-def get_items
-  @TodoArr
-end
+  def get_items
+    @TodoArr
+  end
 
-def get_item(index)
-  @TodoArr[index]
-end
+  def get_item(index)
+    @TodoArr[index]
+  end
 
-def delete_item(item)
-@TodoArr.delete(item)
-end
+  def delete_item(item)
+  @TodoArr.delete(item)
+  end
 
 end

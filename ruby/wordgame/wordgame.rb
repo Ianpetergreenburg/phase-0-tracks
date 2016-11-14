482baaa5947673e14fa089cb616c89b1eb5e5913
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-13 18:22:53 -0800

    finalizing game by adding more instruction for users

--- ruby/wordgame/wordgame.rb
+++ ruby/wordgame/wordgame.rb
@@ -1,97 +1,153 @@
-Alphabet = "abcdefghijklmnopqrstuvwxyz"
-
 class WordGame
-  attr_reader :guesses, :gameboard, :gameover
-  attr_reader :word
+  Alphabet = "abcdefghijklmnopqrstuvwxyz"
+  attr_reader :guesses, :gameboard, :gamewon
+  attr_accessor :word
 
   def initialize(word)
-  @word = word
+    #cast word to 
+  @word = word.downcase
   @wordarr = word.downcase.split('')
-  @guesses = word.length + 2
-  @gameboard = Array.new(word.length){|x| "_"}
+  @guesses = word.length + 5
+  @gameboard = Array.new(word.length){"_"}
   @guessed_letters = []
   @gamewon = false
   end
 
+  #Take in a letter  and return how many times it is in the game word
+  #also update game board to reveal where guessed letter exists in the word
   def guess(letter)
-  correct = false
-  if @guessed_letters.include? letter
-      puts "please guess a letter you have not tried yet" 
-  else
+        letter = letter.downcase
+        num = 0
+        #iterate through word comparing guessed letter to letter's in the word
         @wordarr.each_index do |i| 
                if @wordarr[i] == letter
+                      #if the letter matches update the game board in the equivalent spot
                        @gameboard[i] = letter
-                       correct = true
+                       num += 1
                 end
-            end
+        end
+        #add letter to guessed database, and lower amount of guesses by one
         @guessed_letters << letter
         @guesses -= 1
-    end
-  return correct
+        return num
   end
 
-  def gamewon?
-     @gamewon = (@gameboard.join == @word)
+#Determine if game is won by all letters being guess or whole word guessed
+  def gamewon? (guess = "")
+        @gamewon = (@gameboard == @wordarr || @word == guess)
   end
 
-end
-
-def isletter?(letter)
-    return (Alphabet.include? letter.downcase) && (letter.length == 1)
-end
-
-def isword?(word)
-  word = word.downcase
-  word.each_char { |c| return false if not isletter?(c) }
-  return !word.empty?
-end
-
-#end
-#begins with asking the first user a word to play with
-puts "player1, please enter the word you would like your partner to guess"
-#that word is then accepted and a blank form of the word is created
-gameword = gets.chomp
-
-until isword?(gameword)
-  puts "please only use letters in your word"
-  gameword = gets.chomp
-end
-
-game = WordGame.new(gameword)
-
-while game.guesses > 0
-  puts "Player2, you still have " + game.guesses.to_s + " guesses left."
-  current_guess = gets
-  break if current_guess == nil
-  current_guess = current_guess.chomp
-
-  while !isletter?(current_guess)
-        puts "please only guess with a single letter"
-        current_guess = gets
-        break if current_guess == nil
-        current_guess = current_guess.chomp
+#determine if input is a letter by it's length and if it's in the Alphabet string
+ def isletter?(letter)
+        (Alphabet.include? letter.downcase) && (letter.length == 1)
  end
 
- game.guess(current_guess)
+#determine if word by iterating through string and seeing if each character is a letter
+#return true if the word is made of only letters and isn't an empty string
+ def isword?(word)
+        word = word.downcase
+        word.each_char { |c| return false if not isletter?(c) }
+        !word.empty?
+ end
 
- puts "your gameboard now currently looks like"
+#determine whether or not a given letter has already been gueseed by checking guess database
+ def alreadyguessed? (letter)
+        letter = letter.downcase
+        @guessed_letters.include? letter
+ end
+end
+
+#Ask Users for their name and get Player2 to leave and ask Player1 to give a word to use
+puts "Welcome players to the Fantastic Word Game!"
+puts "Player1 please enter your name"
+player1 = gets.chomp
+puts "Player2 please enter your name"
+player2 = gets.chomp
+puts "\n#{player2}, please step away from the game while #{player1} chooses a word"
+puts "#{player1}, please enter the word you would like your partner to guess"
+
+#take in a word and use it to start the game
+gameword = gets.chomp
+game = WordGame.new(gameword)
+#loop until users input is a string of only letters/not empty 
+until game.isword?(game.word)
+  puts "please only use letters in your word"
+  gameword = gets.chomp
+  game  = WordGame.new(gameword)
+end
+
+#Bring Player2 back and clear the board so they can't see Player1's input
+puts "That's a great word! Once you press enter bring #{player2} back to play!"
+gets
+50.times {puts ""}
+
+#Start Player2's game and prompt for an initial guess
+puts "#{player2}, now is your time to guess #{player1}'s mystery word."
+puts "You have #{game.guesses} chances to guess the letters in it."
+puts "The gameboard currently looks like:"
+puts game.gameboard.join(' ').dump
+puts "Please enter a single letter to make a guess."
+
+#while there are still guesses available accept guesses from user
+while game.guesses > 0
+
+#gather a guess from the user
+#make them guess again if they didn't guess a letter or the letter as been guessed already
+  current_guess = gets.chomp
+  while !game.isletter?(current_guess) || game.alreadyguessed?(current_guess)
+        puts "Please guess only with a single letter" if !game.isletter?(current_guess)
+        puts "Please guess a letter you have not tried yet." if game.alreadyguessed?(current_guess)
+        current_guess = gets.chomp
+ end
+
+#guess the letter and get the amount of times the letter was present
+ revealed = game.guess(current_guess) 
+ #set the guess to correct if any letters were revealed
+correct = revealed > 0
+#congratulate or notify user how many letters they uncovered
+ puts "You revealed #{revealed} letters!" if revealed != 1
+ puts "You revealed #{revealed} letter!" if revealed == 1
+ puts "Awesome!" if revealed > 0
+#show user the current state of the game board
+ puts "\nYour gameboard now currently looks like:" if correct
+ puts "\nYour gameboard is still:" if !correct
  puts game.gameboard.join(' ').dump
 
-  if game.gamewon?
-        puts "Congratulations on guessing the correct word, " + game.word
+#check if the last guess won the game and congratulate the user if so and end the game
+ if game.gamewon?
+        puts "Congratulations on guessing the correct word, #{game.word}"
         break
-  end
+ end
+
+#notify users how many guesses they have left
+ puts "You still have #{game.guesses} guesses left." if game.guesses > 1
+ puts "Make another guess!" if game.guesses > 0
+ #if last guess give possibility to guess the whole word or just another letter
+ if game.guesses == 1
+        puts "Would you like to use your last guess to guess the whole word? (yes/no)"
+        answer = gets.chomp
+        #make sure the user gives a clear yes or no to guessing the whole word
+        until answer == 'yes' or answer == 'no'
+               puts "Please answer yes or no"
+                answer = gets.chomp
+        end
+        #if they want to guess the whole word, check their guess against the game word
+        if answer == 'yes'
+               puts "Please enter what you think the final word might be"
+               guess = gets.chomp.downcase
+               #congratulate the user if they were right
+               if game.gamewon? (guess)
+                       puts "Congratulations on guessing the correct word, #{game.word}"
+               end
+               #end the guesses
+               break
+        end
+        #prompt for a letter if they don't want to guess the whole word
+        puts "Alright guess a letter then!"
+ end
 end
 
-if not game.gamewon?
-  puts "Turns out the word was " + game.word + ", you silly goose. Womp womp."
+#if the user didn't win taunt them
+if not game.gamewon
+  puts "Turns out the word was #{game.word}, you silly goose. Womp womp."
 end
-
-#generate an appropriate amount of guesses dependent on the amount of letters in the word
-#make sure it hasn't been guessed before
-#if it hasn't check if it is in the word
-#if it is then reveal the letter in the blank form
-#--add the letter to a cache of guessed letters
-#--decrease the guess count
-#if game is won give a congratulatory message
-#if guess count hits 0 then give losing message
--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -15,6 +15,10 @@ require_relative 'wordgame'
 describe WordGame do
    let(:wordgame) { WordGame.new("test") }
 
+  it "creates a gameboard equal to length of gameword" do 
+      expect(wordgame.gameboard).to eq ["_","_","_","_"]
+  end
+
    it "takes a guess and returns boolean of if it is in the word" do
       expect(wordgame.guess("t")).to eq true
       expect(wordgame.guess("q")).to eq false
@@ -24,24 +28,20 @@ describe WordGame do
       expect(wordgame.gamewon?).to eq false
   end
 
-  it "creates a gameboard equal to length of gameword" do 
-      expect(wordgame.gameboard).to eq ["_","_","_","_"]
-  end
-
 it "checks if whatever is passed is a string of only letters" do
-  expect(isword?("cc")).to eq true
-  expect(isword?("Test")).to eq true
-  expect(isword?("Test4")).to eq false
-  expect(isword?("c c")).to eq false
-  expect(isword?("")).to eq false
+  expect(wordgame.isword?("cc")).to eq true
+  expect(wordgame.isword?("Test")).to eq true
+  expect(wordgame.isword?("Test4")).to eq false
+  expect(wordgame.isword?("c c")).to eq false
+  expect(wordgame.isword?("")).to eq false
 end
 
   it "checks if whatever is passed is a single letter" do
-   expect(isletter?("c")).to eq true
-   expect(isletter?("bc")).to eq false
-   expect(isletter?(" ")).to eq false
-   expect(isletter?("4")).to eq false
-   expect(isletter?("C")).to eq true
+   expect(wordgame.isletter?("c")).to eq true
+   expect(wordgame.isletter?("bc")).to eq false
+   expect(wordgame.isletter?(" ")).to eq false
+   expect(wordgame.isletter?("4")).to eq false
+   expect(wordgame.isletter?("C")).to eq true
 end
 
 end

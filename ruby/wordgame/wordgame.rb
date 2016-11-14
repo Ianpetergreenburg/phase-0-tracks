29a016876bc7eec08b01e16674021b9c070a3933
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-13 21:17:30 -0800

    fixing pseudocode

--- ruby/wordgame/wordgame.rb
+++ ruby/wordgame/wordgame.rb
@@ -1,4 +1,14 @@
-#pseudocode was broken up and 
+#begins with asking the first user a word to play with
+#that word is then accepted and a blank form of the word is created
+#generate an appropriate amount of guesses dependent on the amount of letters in the word
+#accept a guess letter
+#make sure it hasn't been guessed before
+#if it hasn't check if it is in the word
+#if it is then reveal the letter in the blank form
+#--add the letter to a cache of guessed letters
+#--increase the guess count
+#if game is won give a congratulatory message
+#if guess count hits 0 then give losing message
 
 class WordGame
   Alphabet = "abcdefghijklmnopqrstuvwxyz"
--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -1,48 +1,48 @@
-#begins with asking the first user a word to play with
-#that word is then accepted and a blank form of the word is created
-#generate an appropriate amount of guesses dependent on the amount of letters in the word
-#accept a guess letter
-#make sure it hasn't been guessed before
-#if it hasn't check if it is in the word
-#if it is then reveal the letter in the blank form
-#--add the letter to a cache of guessed letters
-#--increase the guess count
-#if game is won give a congratulatory message
-#if guess count hits 0 then give losing message
-
 require_relative 'wordgame'
 
 describe WordGame do
    let(:wordgame) { WordGame.new("test") }
 
-  it "creates a gameboard equal to length of gameword" do 
+  it "has a gameboard that keeps track of guesses" do 
       expect(wordgame.gameboard).to eq ["_","_","_","_"]
+      wordgame.guess("t")
+      expect(wordgame.gameboard).to eq ["t","_","_", "t"]
   end
 
    it "takes a guess and returns boolean of if it is in the word" do
-      expect(wordgame.guess("t")).to eq true
-      expect(wordgame.guess("q")).to eq false
+      expect(wordgame.guess("t")).to eq 2
+      expect(wordgame.guess("q")).to eq 0
   end
 
   it "keeps track if the game is won" do
-      expect(wordgame.gamewon?).to eq false
+      expect(wordgame.game_won?).to eq false
+      wordgame.guess("t")
+      wordgame.guess("e")
+      wordgame.guess("s")
+      expect(wordgame.game_won?).to eq true
   end
 
 it "checks if whatever is passed is a string of only letters" do
-  expect(wordgame.isword?("cc")).to eq true
-  expect(wordgame.isword?("Test")).to eq true
-  expect(wordgame.isword?("Test4")).to eq false
-  expect(wordgame.isword?("c c")).to eq false
-  expect(wordgame.isword?("")).to eq false
+  expect(wordgame.is_word?("cc")).to eq true
+  expect(wordgame.is_word?("Test")).to eq true
+  expect(wordgame.is_word?("Test4")).to eq false
+  expect(wordgame.is_word?("c c")).to eq false
+  expect(wordgame.is_word?("")).to eq false
 end
 
   it "checks if whatever is passed is a single letter" do
-   expect(wordgame.isletter?("c")).to eq true
-   expect(wordgame.isletter?("bc")).to eq false
-   expect(wordgame.isletter?(" ")).to eq false
-   expect(wordgame.isletter?("4")).to eq false
-   expect(wordgame.isletter?("C")).to eq true
+   expect(wordgame.is_letter?("c")).to eq true
+   expect(wordgame.is_letter?("bc")).to eq false
+   expect(wordgame.is_letter?(" ")).to eq false
+   expect(wordgame.is_letter?("4")).to eq false
+   expect(wordgame.is_letter?("C")).to eq true
+   expect(wordgame.is_letter?("")).to eq false
 end
 
+it "checks if a letter has already been guessed" do
+  expect(wordgame.already_guessed?("t")).to eq false
+  wordgame.guess("t")
+  expect(wordgame.already_guessed?("t")).to eq true
+end
 end
 

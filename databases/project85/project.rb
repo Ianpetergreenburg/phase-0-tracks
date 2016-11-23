a7ac5e2607f035dd360c41d107368e73f1bb56e7
Ian Peter Greenburg <iangreenburg@yahoo.com>
2016-11-22 20:33:01 -0800

    adding create table, drop table, create line and delete line methods

--- databases/project85/project.rb
+++ databases/project85/project.rb
@@ -4,26 +4,29 @@ require "sqlite3"
 db = SQLite3::Database.new("project.db")
 db.results_as_hash = true
 
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS project(
-    id INTEGER PRIMARY KEY,
-    project VARCHAR(255),
-    version VARCHAR(255)
-  );
-SQL
+def create_table_cmd(table)
+ create_table_cmd = <<-SQL
+          CREATE TABLE IF NOT EXISTS #{table}(
+               id INTEGER PRIMARY KEY,
+               project VARCHAR(255),
+               version VARCHAR(255)
+          );
+ SQL
+ db.execute(create_table_cmd)
+end
 
-delete_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS project(
-    id INTEGER PRIMARY KEY,
-    project VARCHAR(255),
-    version VARCHAR(255)
-  );
-SQL
-
-db.execute(create_table_cmd)
+def delete_table_cmd(table)
+ delete_table_cmd =  "DROP TABLE IF EXISTS #{table}"
+ db.execute(delete_table_cmd)
+end
 
 def create_line_item(db, project, version)
   db.execute("INSERT INTO project (project, version) VALUES (?, ?)", [project, version])
+end
+
+def delete_line_item(db,table, id)
+  delete = ("DELETE FROM #{table} WHERE id = #{id}")
+  db.execute(delete)
 end
 
 # 10.times do
@@ -32,6 +35,5 @@ end
 
 projects = db.execute("SELECT * FROM project")
 projects.each do |project|
-  #p project
   puts "Project \##{project["id"]} is called #{project["project"]} and is currently on version #{project["version"]}"
 end
